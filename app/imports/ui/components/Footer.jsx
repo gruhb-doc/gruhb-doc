88277ece53244dc0f3d832ae723cfaaba733a159
Adrian Tam <65895954+adrianwtam@users.noreply.github.com>
2020-11-17 14:11:33 -1000

    Update Footer.jsx
    footer, but I can't tell how it looks if i cant [meteor npm run start]

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { Container, Grid } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { paddingTop: '15px', backgroundColor: "#24292E" };
     return (
-        <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
-            <a href="http://ics-software-engineering.github.io/meteor-application-template-react">Template Home Page</a>
-          </div>
+        <footer color:'white' style:{divStyle}>
+          <Container center aligned>
+            <Grid.Row columns={3}>
+              <Grid.Column>
+                <a>About Us</a>
+                <a>Terms & Service</a>
+                <a>Contact Us</a>
+              </Grid.Column>
+              <Grid.Column>
+                <a>Vendors</a>
+                <a>Suggestions</a>
+                <a>Community</a>
+
+              </Grid.Column>
+              <Grid.Column>
+                <a>Made by [change this in imports/ui/components/Footer.jsx]</a>
+                <a href="https://github.com/gruhb-doc/gruhb-doc"> Github API</a>
+              </Grid.Column>
+            </Grid.Row>
+          </Container>
         </footer>
     );
   }
