f64cc1fe9c7310b31205cee5f85b5058554fce41
Adrian Tam <65895954+adrianwtam@users.noreply.github.com>
2020-11-17 15:47:18 -1000

    Update Footer.jsx
    class fix

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -4,28 +4,28 @@ import { Container, Grid } from 'semantic-ui-react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { color:'white', paddingTop: '15px', backgroundColor: "#24292E" };
+    const divStyle = { color: 'white', paddingTop: '15px', backgroundColor: '#24292E' };
     return (
-        <footer style:{divStyle}>
-          <Container center aligned>
+        <div style={divStyle}>
+          <Container center aligned inverted>
             <Grid.Row columns={3}>
               <Grid.Column>
-                <a>About Us</a>
-                <a>Terms & Service</a>
-                <a>Contact Us</a>
+                <Grid.Item>About Us</Grid.Item>
+                <Grid.Item>Terms of Service</Grid.Item>
+                <Grid.Item>Contact Us</Grid.Item>
               </Grid.Column>
               <Grid.Column>
-                <a>Vendors</a>
-                <a>Suggestions</a>
-                <a>Community</a>
+                <Grid.Item>Vendors</Grid.Item>
+                <Grid.Item>Suggestions</Grid.Item>
+                <Grid.Item>Community</Grid.Item>
               </Grid.Column>
               <Grid.Column>
-                <a>GrUHb(2020)©</a>
-                <a href="https://github.com/gruhb-doc/gruhb-doc"> Github API</a>
+                <Grid.Item>GrUHb(2020)©</Grid.Item>
+                <Grid.Item href="https://github.com/gruhb-doc/gruhb-doc">Github API</Grid.Item>
               </Grid.Column>
             </Grid.Row>
           </Container>
-        </footer>
+        </div>
     );
   }
 }
